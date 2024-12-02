748aadb0e6f66ac4bd44dc2b5821ec82dd4c58a2
Wibaek Park <devmuromi@gmail.com>
2024-12-02 16:17:07 +0900

    feat: MaterialApp와 Scaffold를 사용하는 기본 구조 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello, World!"),
+        ),
+        body: Center(
+          child: Text("Hello, World!"),
+        ),
+      ),
+    );
+  }
 }
